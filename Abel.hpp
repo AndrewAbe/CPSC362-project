93cc80218f5c8a893ca123a4b7df355b6da9a4b3
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-09-06 16:31:10 -0700

    Update Abel.hpp

--- Abel.hpp
+++ Abel.hpp
@@ -4,13 +4,18 @@
 class Item {
 public:
     Item() {
+        _SellPrice = 0.5;
         _PurchaseCost = 0.01;
         _Quantity = 100;
     };
-    Item(float x, float y) {
-        _PurchaseCost = x;
-        _Quantity = y;
+    Item(float x, float y, float z) {
+        _SellPrice = x;
+        _PurchaseCost = y;
+        _Quantity = z;
     };
+    float getSellPrice() {
+        return _SellPrice;
+    }
     float getPurchaseCost() {
         return _PurchaseCost;
     }
@@ -19,6 +24,7 @@ public:
     }
 
 private:
+    float _SellPrice;
     float _PurchaseCost;
     float _Quantity;
 };
