87ac8cbd5742949c9c09f4457d91671cf2d4c9c0
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-09-06 16:30:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,15 +11,17 @@ using namespace std;
 // Main
 int main() {
 
-    Item Orange (1, 1);
+    Item Orange (1, 1, 1);
+    float h = Orange.getSellPrice();
     float i = Orange.getPurchaseCost();
     float j = Orange.getQuantity();
-    cout << "Orange is purchase cost " << i << " and has quantity of " << j << "\n";
+    cout << "Orange is sold at " << h << " purchased at " << i << " and has quantity of " << j << "\n";
 
     Item Lemon;
-    float a = Lemon.getPurchaseCost();
-    float b = Lemon.getQuantity();
-    cout << "Orange is purchase cost " << a << " and has quantity of " << b << "\n";
+    float a = Lemon.getSellPrice();
+    float b = Lemon.getPurchaseCost();
+    float c = Lemon.getQuantity();
+    cout << "Lemon is sold at " << a << " purchased at " << b << " and has quantity of " << c << "\n";
 
     return 0;
 }
