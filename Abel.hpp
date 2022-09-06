71925767d4859a0cbbb5b99b3aa421734e244c85
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-09-06 15:49:37 -0700

    Update Abel.hpp

--- Abel.hpp
+++ Abel.hpp
@@ -2,3 +2,18 @@
 #include <string>
 #include <map>
 
+template <typename Key, typename Value>
+class Item {
+public:
+    Item() = default;
+    float getPurchaseCost() {
+        return _PurchaseCost();
+    }
+    float getQuantity() {
+        return _Quantity();
+    }
+
+private:
+    float _PurchaseCost = '1';
+    float _Quantity = '10';
+};
