51d7df03203569e7a750f83372508e6239a18f00
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-09-15 13:47:08 -0700

    Update Abel.hpp

--- Abel.hpp
+++ Abel.hpp
@@ -1,4 +1,42 @@
 #include <iostream>
 #include <string>
-#include <map>
 
+class Item {
+public:
+    //constructors
+    Item() {
+        _SellPrice = 0.5;
+        _PurchaseCost = 0.01;
+        _Quantity = 100;
+    };
+    Item(double x, double y, double z) {
+        _SellPrice = x;
+        _PurchaseCost = y;
+        _Quantity = z;
+    };
+    //get private variables
+    double getSellPrice() {
+        return _SellPrice;
+    }
+    double getPurchaseCost() {
+        return _PurchaseCost;
+    }
+    double getQuantity() {
+        return _Quantity;
+    }
+    //set functions
+    void setSellPrice(double price) {
+        _SellPrice = price;
+    };
+    void setPurchaseCost(double cost) {
+        _PurchaseCost = cost;
+    };
+    void setQuantity(double quantity) {
+        _Quantity = quantity;
+    };
+
+private:
+    double _SellPrice;
+    double _PurchaseCost;
+    double _Quantity;
+};
