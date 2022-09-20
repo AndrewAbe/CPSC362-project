5ac991639b8ed3802ec317e42de934e7edc903d7
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-09-19 18:23:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -47,24 +47,15 @@ int main() {
         cout << "\n";
     }
 
-    Item Orange (1, 1, 1);
-    double h = Orange.getSellPrice();
-    double i = Orange.getPurchaseCost();
-    double j = Orange.getQuantity();
-    cout << "Orange is sold at " << h << " purchased at " << i << " and has quantity of " << j << "\n";
+    Item Orange(1, 1, 1);
+    Orange.print();
 
     Item Lemon;
-    double a = Lemon.getSellPrice();
-    double b = Lemon.getPurchaseCost();
-    double c = Lemon.getQuantity();
-    cout << "Lemon is sold at " << a << " purchased at " << b << " and has quantity of " << c << "\n";
+    Lemon.print();
     Lemon.setSellPrice(111.0);
     Lemon.setPurchaseCost(111.0);
     Lemon.setQuantity(111.0);
-    a = Lemon.getSellPrice();
-    b = Lemon.getPurchaseCost();
-    c = Lemon.getQuantity();
-    cout << "Lemon is sold at " << a << " purchased at " << b << " and has quantity of " << c << "\n";
+    Lemon.print();
 
     return 0;
 }
