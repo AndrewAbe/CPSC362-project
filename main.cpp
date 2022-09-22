35b5d18975fc586f61f3b62eb7dd06c23579770b
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-09-22 13:49:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,33 +17,11 @@ int main() {
     //cout << "Enter the file name: ";
     //cin >> fname;
     
-    //openexcel(fname, list);
-    //<Item> item1 = list.at(0);
+    openexcel(fname, list);
+    //Item item1 = list.at(0);
     //item1.print();
+    printVec(list);
 
-    string line;
-
-    fstream file(fname, ios::in);
-    if (file.is_open()) {
-        //get first line to skip since it has headers
-        getline(file, line);
-        string ItemName, SellPrice, PurPrice, Inventory, TotalName, ActualTotalNum; //last 2 are for inherented function
-        //cout << line;  //works
-        while (getline(file, line)) {
-            stringstream str(line);
-            getline(str, ItemName, ',');
-            getline(str, SellPrice, ',');
-            getline(str, PurPrice, ',');
-            getline(str, Inventory, ',');
-            Item itemi(ItemName, stod(SellPrice), stod(PurPrice), stod(Inventory));
-            list.push_back(itemi);
-        }
-        //cout << "list size " << list.size() << "\n";
-        printVec(list); 
-    }
-    else {
-        cout << "Could not open the file\n";
-    }
     return 0;
 }
 
