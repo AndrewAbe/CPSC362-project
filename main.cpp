df40ed298beb3a5080cbadcb1df34defd364c695
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-09-21 20:08:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,65 +6,63 @@
 
 #include "Abel.hpp"
 
+#pragma once 
+
 using namespace std;
 
-//kivy
-
-// Main
 int main() {
+    string fname = "Fruit_Inventory.csv";
+    vector<Item> list;
+    //hardcode for ease can change back later
+    //cout << "Enter the file name: ";
+    //cin >> fname;
+    
+    //openexcel(fname, list);
+    //<Item> item1 = list.at(0);
+    //item1.print();
 
-    string fname;
-    cout << "Enter the file name: ";
-    cin >> fname;
-
-    vector<vector<string>> content;
-    vector<string> row;
-    string line, word;
+    string line;
 
     fstream file(fname, ios::in);
-    if (file.is_open())
-    {
-        while (getline(file, line))
-        {
-            row.clear();
-
+    if (file.is_open()) {
+        //get first line to skip since it has headers
+        getline(file, line);
+        string ItemName, SellPrice, PurPrice, Inventory, TotalName, ActualTotalNum; //last 2 are for inherented function
+        //cout << line;  //works
+        while (getline(file, line)) {
             stringstream str(line);
-
-            while (getline(str, word, ','))
-                row.push_back(word);
-            content.push_back(row);
+            getline(str, ItemName, ',');
+            getline(str, SellPrice, ',');
+            getline(str, PurPrice, ',');
+            getline(str, Inventory, ',');
+            Item itemi(ItemName, stod(SellPrice), stod(PurPrice), stod(Inventory));
+            list.push_back(itemi);
         }
+        //cout << "list size " << list.size() << "\n";
+        printVec(list); 
     }
-    else
+    else {
         cout << "Could not open the file\n";
-
-    for (int i = 0; i < content.size(); i++)
-    {
-        for (int j = 0; j < content[i].size(); j++)
-        {
-            cout << content[i][j] << " ";
-        }
-        cout << "\n";
     }
+    return 0;
+}
 
-    Item Orange (1, 1, 1);
-    double h = Orange.getSellPrice();
-    double i = Orange.getPurchaseCost();
-    double j = Orange.getQuantity();
-    cout << "Orange is sold at " << h << " purchased at " << i << " and has quantity of " << j << "\n";
 
+
+/* OLD TESTS
+
+//Works for classes and changes to classes
+    Item Orange("Orange", 1, 1, 1);
+    Orange.print();
     Item Lemon;
-    double a = Lemon.getSellPrice();
-    double b = Lemon.getPurchaseCost();
-    double c = Lemon.getQuantity();
-    cout << "Lemon is sold at " << a << " purchased at " << b << " and has quantity of " << c << "\n";
+    Lemon.print();
     Lemon.setSellPrice(111.0);
     Lemon.setPurchaseCost(111.0);
     Lemon.setQuantity(111.0);
-    a = Lemon.getSellPrice();
-    b = Lemon.getPurchaseCost();
-    c = Lemon.getQuantity();
-    cout << "Lemon is sold at " << a << " purchased at " << b << " and has quantity of " << c << "\n";
+    Lemon.setName("Lemon");
+    Lemon.print();
 
-    return 0;
-}
+
+
+
+*/
