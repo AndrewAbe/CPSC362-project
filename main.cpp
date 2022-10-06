349576628bd88308aca3c8c5e7d7284ec92d5de8
AndrewAbe <78701587+AndrewAbe@users.noreply.github.com>
2022-10-06 13:57:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,25 @@
 
 #pragma once 
 
+//UML
+
 using namespace std;
 
 int main() {
     string fname = "Fruit_Inventory.csv";
     vector<Item> list;
+    double tot = 0.0;
     //hardcode for ease can change back later
     //cout << "Enter the file name: ";
     //cin >> fname;
     
-    openexcel(fname, list);
+    openexcel(fname, list, tot);
     //Item item1 = list.at(0);
     //item1.print();
+    cout << "Total: " << tot << "\n";
     printVec(list);
+
+    endexcel(fname, list, tot);
 
     return 0;
 }
