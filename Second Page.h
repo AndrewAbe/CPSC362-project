f0d0b9c465b8741d49b7476495e4b3bc2d5c56b0
Cristian1102 <100620851+Cristian1102@users.noreply.github.com>
2022-10-06 13:59:57 -0700

    Update Second Page.h

--- Second Page.h
+++ Second Page.h
@@ -16,19 +16,20 @@ int secondpage()
 {
     string fname = "Fruit_Inventory.csv";
     vector<Item> list;
+    
+    double x;
 
-    openexcel(fname, list);
+    openexcel(fname, list, x);
 
     
-    std::cout << "Please select the Item you wish to change the price for:\n\n";
-    
-    std::cout << "1.Apples\n2.Bananas\n3.Blueberries\n4.Grapes\n5.Lemons\n6.Oranges\n7.Mangos\n8.Watermelons\n";
+    std::cout << "Please select the Item you wish to change the price for or input 0 to go back to the homepage:\n\n";
+    //update 
+    std::cout << "1.Apples\n2.Bananas\n3.Blueberries\n4.Grapes\n5.Lemons\n6.Oranges\n7.Mangos\n8.Watermelons\n9.Twinkies\n10.Beef Jerky\n11.Canned Beans\n12.Peanuts";
    
 
     cout << "\n\n\n\n";
 
-
-    std::cout << "To return to the homepage, please input zero: ";
+    std::cout << "Please input your selection: ";
 
     int selection;
     double value;
@@ -41,36 +42,97 @@ int secondpage()
         temp = list.at(0);
         cin >> value; 
         temp.setSellPrice(value);
-        return main();
+        return secondpage();
     }
     if (selection == 2) {
-        return main();
+        cout << "Please input the new price for each Bananas: " << endl;
+        temp = list.at(1);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
     }
     if (selection == 3) {
-        return main();
+        cout << "Please input the new price for each Blueberries: " << endl;
+        temp = list.at(2);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
     }
     if (selection == 4) {
-        return main();
+        cout << "Please input the new price for each Grapes: " << endl;
+        temp = list.at(3);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
     }
     if (selection == 5) {
-        return main();
+        cout << "Please input the new price for each Lemons: " << endl;
+        temp = list.at(4);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
     }
     if (selection == 6) {
-        return main();
+        cout << "Please input the new price for each Oranges: " << endl;
+        temp = list.at(5);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
     }
     if (selection == 7) {
-        return main();
-    }
-    if (selection == 8) {
-        return main();
+        cout << "Please input the new price for each Mangos: " << endl;
+        temp = list.at(6);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
     }
 
-    while (selection >= 9) {
+    if (selection == 8) {
+        cout << "Please input the new price for each Watermelons: " << endl;
+        temp = list.at(7);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
+    }
+
+    if (selection == 9) {
+        cout << "Please input the new price for each Twinkies: " << endl;
+        temp = list.at(8);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
+    }
+
+    if (selection == 10) {
+        cout << "Please input the new price for each Beef Jerky: " << endl;
+        temp = list.at(9);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
+    }
+        
+    if (selection == 11) {
+        cout << "Please input the new price for each Canned Beans: " << endl;
+        temp = list.at(10);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
+    }
+
+    if (selection == 12) {
+        cout << "Please input the new price for each Peanuts: " << endl;
+        temp = list.at(11);
+        cin >> value;
+        temp.setSellPrice(value);
+        return secondpage();
+    }
+
+    while (selection >= 13) {
         cout << "Invalid selection, to return to the homepage, please input 0: ";
         std::cin >> selection;
     }
 
     cout << "\n\n\n\n\n\n\n\n";
-
+    
     return main();
 }
